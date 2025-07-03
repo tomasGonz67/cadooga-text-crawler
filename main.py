dfe0693abcf581e5e7f88f4c47e5b7c01d295ff0
Tomas Gonzalez <tpg3@njit.edu>
2025-07-03 16:08:07 -0400

    testing again

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class HealthResponse(BaseModel):
 @app.get("/")
 async def root():
     """Root endpoint"""
-    return {"message": "Text Crawler API is running", "version": "cuck  hold 3.1"}
+    return {"message": "Text Crawler API is running", "version": "cuck  hold 3.2"}
 
 @app.get("/health", response_model=HealthResponse)
 async def health_check():
